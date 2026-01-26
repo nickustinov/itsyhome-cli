6a4c53ab2babb432ca673cdd4d43116e6dbf3436
Nick Ustinov <nickustinov@gmail.com>
2026-01-26 10:52:25 +0000

    Update Formula sha256 for v0.3.0

--- Formula/itsyhome.rb
+++ Formula/itsyhome.rb
@@ -2,7 +2,7 @@ class Itsyhome < Formula
   desc "CLI tool to control HomeKit devices via Itsyhome"
   homepage "https://github.com/nickustinov/itsyhome-cli"
   url "https://github.com/nickustinov/itsyhome-cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 ""
+  sha256 "e0a853e65e8f9d4254bce331022da9e1d3961253da1313dc753acc71dbc6e345"
   license "MIT"
 
   depends_on "go" => :build
